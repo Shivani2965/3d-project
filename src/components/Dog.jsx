2ddfdd400fdd830821d428d057a2fb12e03f45f6
shivani2965 <shivanidevanekar2965@gmail.com>
2026-01-11 20:47:37 +0530

    adding animations

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import { Canvas , useThree} from '@react-three/fiber'
-import { OrbitControls, useGLTF } from '@react-three/drei'
+import * as THREE from 'three'
+import { useEffect } from 'react'
+import { useGLTF, useAnimations, OrbitControls } from '@react-three/drei'
+import { useThree } from '@react-three/fiber'
 
-const Dog = () => {
-    const scene = useGLTF("/models/Fish.glb")
+const Fish = () => {
+  const { scene, animations } = useGLTF('/models/Fish.glb')
+  const { actions } = useAnimations(animations, scene)
+  const { camera } = useThree()
+
+  // Set camera once
+  useEffect(() => {
+    camera.position.set(0, 2, 10)
+    camera.lookAt(0, 0, 0)
+  }, [camera])
+
+  // Play animation (like a GIF loop)
+  useEffect(() => {
+    if (actions) {
+      const firstAction = Object.values(actions)[0]
+      firstAction?.reset().fadeIn(0.5).play()
+    }
+  }, [actions])
 
   return (
     <>
-    <primitive object={scene.scene}  />
-        <OrbitControls />
-        <directionalLight position={[10,10,5]} intensity={1} />
-      </>
-    
+      <primitive
+        object={scene}
+        scale={1.5}
+        position={[0, 0, 0]}
+        rotation={[0, -Math.PI / 4, 0]}
+      />
+
+      <directionalLight position={[5, 5, 5]} intensity={2} />
+      <ambientLight intensity={0.5} />
+      <OrbitControls enableZoom={false} />
+    </>
   )
 }
 
-export default Dog
+export default Fish
